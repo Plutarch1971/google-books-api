b719d15465c6e98e5c171ba3098aa66dc0828c87
Matt-Verni <matthewpmendez@gmail.com>
2024-11-30 18:45:16 -0500

    Modified connection.ts

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const openDB = async (): Promise<typeof mongoose.connection> =>{
     try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googleBookdDB');
+        await mongoose.connect(process.env.MONGODB_URI || '');
         console.log('Database connected.');
         return mongoose.connection;
     } catch(error) {
