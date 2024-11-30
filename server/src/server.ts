eb407456e5af8d3a6f1ce7660a68ac1acce7e08c
Matt-Verni <matthewpmendez@gmail.com>
2024-11-30 18:55:41 -0500

    Modified server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import db from './config/connection.js';
 import path from 'node:path';
+import { fileURLToPath } from 'url';
 import { ApolloServer } from '@apollo/server';
 
 import { expressMiddleware } from '@apollo/server/express4';
 import { typeDefs, resolvers } from './schemas/index.js';
 import { authenticateToken } from './services/auth.js';
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const server = new ApolloServer({
   typeDefs, 
   resolvers,
